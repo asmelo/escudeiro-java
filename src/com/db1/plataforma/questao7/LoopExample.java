9d9d851345bb77b99ac4b858050c49d286003161
Alexandre Simões de Melo <alexandre.melo@db1.com.br>
2020-09-07 11:58:23 -0300

    Questao 7

---
+++ src/com/db1/plataforma/questao7/LoopExample.java
@@ -0,0 +1,96 @@
+package com.db1.plataforma.questao7;
+
+import javax.swing.plaf.IconUIResource;
+import java.util.*;
+
+public class LoopExample {
+
+    public static void main(String[] args) {
+        //Exemplo do for
+        String[] names = {"Arnaldo", "Priscila", "Jonas", "Marta"};
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(String.format("Na posição %d está o nome %s", i, names[i]));
+        }
+
+        //Exemplo do enhanced for
+        String[] mainTeams = {"Flamento", "São Paulo", "Vasco", "Palmeiras", "Corinthians"};
+        StringJoiner stringJoiner = new StringJoiner(", ", "[", "]");
+        for (String team : mainTeams) {
+            stringJoiner.add(String.valueOf(team));
+        }
+        System.out.println("\nO principais times são: " + stringJoiner.toString());
+
+
+        //Exemplo do while
+        List<Integer> numbers = new ArrayList<>();
+
+        numbers.add(4);
+        numbers.add(1);
+        numbers.add(7);
+        numbers.add(2);
+        numbers.add(9);
+        numbers.add(1);
+        numbers.add(5);
+
+        Collections.sort(numbers);
+
+        int count = 0;
+        Integer value = numbers.get(count);
+        System.out.println();
+        while (value < 5 && count < numbers.size()) {
+            System.out.println("Valor menor que 5 encontrado: " + numbers.get(count));
+            value = numbers.get(++count);
+        }
+
+        //Exemplo do do/while
+        Random random = new Random();
+        int randomInt = random.nextInt(20);
+        System.out.println();
+        do {
+            System.out.println("Número sorteado: " + randomInt);
+            randomInt = random.nextInt(20);
+        } while (randomInt < 15);
+
+        //Exemplo do forEach do Interable
+        stringJoiner = new StringJoiner(", ", "[", "]");
+        List<String> cars = List.of("Gol", "Ka", "Sentra", "Siena", "Golf");
+        cars.forEach(stringJoiner::add);
+        System.out.println("\nLista de carros: " + stringJoiner.toString());
+
+
+        //Exemplo de uso do break
+        //Neste exemplo temos um laço cuja expressão lógica é sempre verdadeira, para finaliza o laço é utilizando
+        //o comando break que força a interrupção do laço, independente da expressão lógica.
+        Double number, sum = 0.0;
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            System.out.print("Digite um número paraa ser somado. Para finalizar digite 0.: ");
+            number = input.nextDouble();
+            if (number == 0) {
+                break;
+            }
+            sum += number;
+        }
+        System.out.println("O resultado da soma é: " + sum);
+
+        //Exemplo de uso do continue
+        //No exemplo abaixo é feita uma contagem regressiva a partir de 10 onde são impressos apenas os números pares.
+        //Para isso foi utilizado o comando "continue" que não finaliza a execução do laço mas interrompe a interação
+        //no momento em que é executado. No exemplo caso seja um número ímpar a interação é interrompida impedindo
+        //assim que o número seja impresso.
+        System.out.println("\nContagem regressiva par: ");
+        int counter=10;
+        while (counter >=0)
+        {
+            if (counter % 2 != 0)
+            {
+                counter--;
+                continue;
+            }
+            System.out.print(counter+" ");
+            counter--;
+        }
+
+    }
+
+}
---
+++ src/com/db1/plataforma/questao7/resposta-questao7.md
@@ -0,0 +1,14 @@
+**Faça uma comparação entre os tipos de laços**
+
+O "for" simples do Java é composto por três parâmetros: inicialização, expressão lógica e uma ação a ser executada a 
+cada passo, geralmente um incremento. São parâmetros opcionais que irão controlar a execução do laço. O "enhanced for"
+por sua vez possui uma estrutura mais simples. Este possui a declaração de uma variável que irá conter cada um dos 
+itens do array/colletion e o array/colletion em si. Apesar de ser mais simples e legível pode não ser adequado para
+todos os casos, por exemplo quando precisa-se acessar o índice que indica as posições do array/colletion é preciso 
+usar o "for" simples.    
+
+
+O "while" possui uma outra lógica de execução em relação ao "for", apenas uma expressão lógica irá determinar se a 
+execução do laço irá parar ou não. O "do/while" se comporta de forma similar é diferença é que a primeira execução
+do laço sempŕe irá ocorrer, ao contrário do "while" que pode não realizar interação alguma caso a expressão lógica já
+no primeiro momento seja falsa. 
